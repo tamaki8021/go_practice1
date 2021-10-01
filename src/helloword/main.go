05a8a9f875edd1090a8cebcebdb89f850b382e01
hbk2002320@gmail.com <hbk2002320@gmail.com>
2021-10-01 22:08:31 +0900

    :sparkles: 例外処理

--- src/helloword/main.go
+++ src/helloword/main.go
@@ -2,22 +2,18 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"os"
-	// "io"
-	// "os"
 )
 
 func main()  {
 	// 例外処理
-	// defer func()  {
-	// 	handler := recover()
-	// 		if handler != nil {
-	// 			fmt.Println("main(): recover", handler)
-	// 		}
-	// }()
+	defer func()  {
+		handler := recover()
+			if handler != nil {
+				fmt.Println("main(): recover", handler)
+			}
+	}()
 
-	createfile()
+	// createfile()
 
 	// highlow(2, 0)
 	// fizzBuzz()
@@ -31,17 +27,17 @@ func main()  {
 }
 
 // 例外処理
-// func highlow(high int, low int)  {
-// 	if high < low {
-// 		fmt.Println("Panic!")
-// 		panic("highlow() low greater than high")
-// 	}
+func highlow(high int, low int)  {
+	if high < low {
+		fmt.Println("Panic!")
+		panic("highlow() low greater than high")
+	}
 
-// 	defer fmt.Println("Deferred: highlow(", high, ",", low, ",")
-// 	fmt.Println("Call: highlow(", high, ",", low, ")")
+	defer fmt.Println("Deferred: highlow(", high, ",", low, ",")
+	fmt.Println("Call: highlow(", high, ",", low, ")")
 
-// 	highlow(high, low + 1)
-// }
+	highlow(high, low + 1)
+}
 
 // func fizzBuzz()  {
 // 	for i := 1; i < 100; i++ {
@@ -71,17 +67,17 @@ func main()  {
 // 	}
 // }
 
-func createfile()  {
-	newfile, error := os.Create("learnGo.txt")
-	if error != nil {
-		fmt.Println("Error: Could not create file.")
-		return
-	}
-	defer newfile.Close()
+// func createfile()  {
+// 	newfile, error := os.Create("learnGo.txt")
+// 	if error != nil {
+// 		fmt.Println("Error: Could not create file.")
+// 		return
+// 	}
+// 	defer newfile.Close()
 
-	if _, error = io.WriteString(newfile, "Learning Go!"); error != nil {
-		fmt.Println("Error: Could not write to file.")
-	}
+// 	if _, error = io.WriteString(newfile, "Learning Go!"); error != nil {
+// 		fmt.Println("Error: Could not write to file.")
+// 	}
 
-	newfile.Sync()
-}
+// 	newfile.Sync()
+// }
