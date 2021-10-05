8154780c857b5469ab80ddb1378b8cd258aff76b
hbk2002320@gmail.com <hbk2002320@gmail.com>
2021-10-05 21:30:00 +0900

    feat: array

--- src/helloword/main.go
+++ src/helloword/main.go
@@ -15,6 +15,35 @@ func main()  {
 			}
 	}()
 
+	array()
+	arrays()
+}
+
+func arrays()  {
+	var twoD [3][5]int
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 5; j++ {
+			twoD[i][j] = (i + 1) * (j + 1)
+		}
+		fmt.Println("Row", i, twoD[i])
+	}
+
+	fmt.Println("\nAll at once:", twoD)
+}
+
+func array()  {
+	var a [4]int
+	a[1] = 10
+	fmt.Println(a[0])
+	fmt.Println(a[1])
+	fmt.Println(len(a))
+	fmt.Println(a[len(a) -1])
+
+	cities := [...]string{"New York", "Paris", "Berlin", "Madrid"}
+	fmt.Println("Cities:", cities)
+
+	numbers := [...]int{99: -1}
+	fmt.Println("First Position:", numbers[99])
 }
 
 func panic() string {
