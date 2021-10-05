f25b60fce3cb8f0d666a5c7d98ad884a74b7fed1
hbk2002320@gmail.com <hbk2002320@gmail.com>
2021-10-05 22:33:17 +0900

    feat: fibonacci

--- src/helloword/main.go
+++ src/helloword/main.go
@@ -16,7 +16,26 @@ func main()  {
 			}
 	}()
 
-	employee()
+	var num int
+
+	fmt.Scanln(&num)
+	fibonacci(num)
+}
+
+// フィボナッチ配列
+func fibonacci(n int) []int {
+	if n < 2 {
+		return make([]int, 0)
+	}
+
+	nums := make([]int, n)
+	nums[0], nums[1] = 1, 1
+
+	for i := 2; i < n; i++ {
+		nums[i] = nums[i - 1] + nums[i - 2]
+	}
+
+	return nums
 }
 
 type Parson struct {
