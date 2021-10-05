f2080a24f0a8ea30b61c3f7574b4bf2924b56da1
hbk2002320@gmail.com <hbk2002320@gmail.com>
2021-10-05 21:15:47 +0900

    fix

--- src/helloword/main.go
+++ src/helloword/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 )
 
 func main()  {
@@ -13,91 +15,78 @@ func main()  {
 			}
 	}()
 
-	// createfile()
-	// highlow(2, 0)
-	// fizzBuzz()
-	// fmt.Println("Program number less than 20")
-
-	// for number := 1; number <= 20; number++ {
-	// 	if findprimes(number) {
-	// 		fmt.Printf("%v ", number)
-	// 	}
-	// }
-
-	// panic()
 }
 
-// func panic() string {
-// 	val := 0
+func panic() string {
+	val := 0
 
-// 	for {
-// 			fmt.Print("Enter number: ")
-// 			fmt.Scanf("%d", &val)
+	for {
+			fmt.Print("Enter number: ")
+			fmt.Scanf("%d", &val)
 
-// 			switch {
-// 			case val < 0:
-// 				fmt.Println("Panic!")
-// 				panic("You entered a negative number!")
-// 			case val == 0:
-// 					fmt.Println("0 is neither negative nor positive")
-// 			default:
-// 					fmt.Println("You entered:", val)
-// 			}
-// 	}
-// }
+			switch {
+			case val < 0:
+				// panic("You entered a negative number!")
+			case val == 0:
+					fmt.Println("0 is neither negative nor positive")
+			default:
+					fmt.Println("You entered:", val)
+			}
+	}
+}
 
 // 例外処理
-// func highlow(high int, low int)  {
-// 	if high < low {
-// 		fmt.Println("Panic!")
-// 		panic("highlow() low greater than high")
-// 	}
+func highlow(high int, low int)  {
+	if high < low {
+		fmt.Println("Panic!")
+		// panic("highlow() low greater than hig")
+	}
 
-// 	defer fmt.Println("Deferred: highlow(", high, ",", low, ",")
-// 	fmt.Println("Call: highlow(", high, ",", low, ")")
+	defer fmt.Println("Deferred: highlow(", high, ",", low, ",")
+	fmt.Println("Call: highlow(", high, ",", low, ")")
 
-// 	highlow(high, low + 1)
-// }
+	highlow(high, low + 1)
+}
 
-// func fizzBuzz()  {
-// 	for i := 1; i < 100; i++ {
-// 		switch {
-// 		case i % 3 == 0 && i % 5 == 0:
-// 			fmt.Println("FizzBuzz")
-// 		case i % 3 == 0:
-// 			fmt.Println("Fizz")
-// 		case i % 5 == 0:
-// 			fmt.Println("Buzz")
-// 		default:
-// 			fmt.Println(i)
-// 		}
-// 	}
-// }
+func fizzBuzz()  {
+	for i := 1; i < 100; i++ {
+		switch {
+		case i % 3 == 0 && i % 5 == 0:
+			fmt.Println("FizzBuzz")
+		case i % 3 == 0:
+			fmt.Println("Fizz")
+		case i % 5 == 0:
+			fmt.Println("Buzz")
+		default:
+			fmt.Println(i)
+		}
+	}
+}
 
-// func findprimes(number int) bool {
-// 	for i := 2; i < number; i++ {
-// 		if number % i == 0 {
-// 			return false
-// 		}
-// 	}
-// 	if number > 1 {
-// 		return true
-// 	} else {
-// 		return false
-// 	}
-// }
+func findprimes(number int) bool {
+	for i := 2; i < number; i++ {
+		if number % i == 0 {
+			return false
+		}
+	}
+	if number > 1 {
+		return true
+	} else {
+		return false
+	}
+}
 
-// func createfile()  {
-// 	newfile, error := os.Create("learnGo.txt")
-// 	if error != nil {
-// 		fmt.Println("Error: Could not create file.")
-// 		return
-// 	}
-// 	defer newfile.Close()
+func createfile()  {
+	newfile, error := os.Create("learnGo.txt")
+	if error != nil {
+		fmt.Println("Error: Could not create file.")
+		return
+	}
+	defer newfile.Close()
 
-// 	if _, error = io.WriteString(newfile, "Learning Go!"); error != nil {
-// 		fmt.Println("Error: Could not write to file.")
-// 	}
+	if _, error = io.WriteString(newfile, "Learning Go!"); error != nil {
+		fmt.Println("Error: Could not write to file.")
+	}
 
-// 	newfile.Sync()
-// }
+	newfile.Sync()
+}
